d974d2b7cc4ae6ee8dd697a5ca754ff0dffbc37f
Edward Paget <ed.paget@gmail.com>
2013-08-20 13:33:08 -0500

    Use DATABASE_URL env var

--- src/zoo_live/system.clj
+++ src/zoo_live/system.clj
@@ -17,7 +17,7 @@
     {:redis-pub-sub {:pool {} 
                      :listener nil
                      :spec {:uri redis-pub-sub}}
-     :postgres (get env "HEROKU_POSTGRESQL_PURPLE_URL")
+     :postgres (get env "DATABASE_URL")
      :redis {:pool {} :spec {:uri redis}}
      :handler (r/routes)
      :port 8080}))
