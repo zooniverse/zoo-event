0ae6cb634638441aa4dd88645d8c2043bc88432a
Edward Paget <ed.paget@gmail.com>
2014-02-19 12:51:19 -0600

    Add korma clean up used deps

--- project.clj
+++ project.clj
@@ -2,16 +2,13 @@
   :description "Live Zooniverse Classifications"
   :url "http://github.com/edpaget/zoo-live"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.cemerick/url "0.0.8"]
-                 [org.clojure/data.json "0.2.2"]
                  [compojure "1.1.5"]
                  [ring/ring-json "0.2.0"]
                  [http-kit "2.1.16"]
-                 [clj-time "0.6.0"]
-                 [clj-http "0.7.7"]
                  [clj-kafka "0.1.2-0.8"]
                  [org.clojure/core.match "0.2.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
+                 [korma "0.3.0-rc5"]
                  [postgresql/postgresql "8.4-702.jdbc4"]]
   :profiles
   {:dev {:source-paths ["dev"]
