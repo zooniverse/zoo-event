c33dc4a8ffb3291e26bdbb9cf432b56bf201ff82
Edward Paget <edward@cassetteta.pe>
2014-05-07 13:42:24 -0500

    Update project and license

--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
-(defproject zoo-live "0.4.1-SNAPSHOT"
+(defproject zoo-event "1.0.0-SNAPSHOT"
   :description "Live Zooniverse Classifications"
-  :url "http://github.com/edpaget/zoo-live"
-  :main zoo-live.system
+  :url "http://github.com/zooniverse/zoo-events"
+  :license {:name "Apache Public License v2"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+  :main zoo-event.system
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [ring/ring-json "0.2.0"]
+                 [ring/ring-json "0.3.1"]
                  [http-kit "2.1.16"]
-                 [clj-kafka "0.2.0-0.8"]
+                 [clj-kafka "0.2.4-0.8"]
                  [clj-time "0.6.0"]
-                 [pg-json "0.1.0-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/math.combinatorics "0.0.7"]
-                 [org.clojure/java.jdbc "0.3.0-alpha4"]
-                 [korma "0.3.0-RC5"]
-                 [postgresql/postgresql "8.4-702.jdbc4"]]
+                 [com.stuartsierra/component "0.2.1"]
+                 [pg-json "0.2.0"]
+                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [korma "0.3.1"]
+                 [postgresql/postgresql "9.3-1101.jdbc4"]]
   :profiles
   {:dev {:source-paths ["dev"]
          :dependencies [[ring-mock "0.1.5"]
