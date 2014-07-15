a845019d052fe6a43a03730cebece4013ba7897d
Edward Paget <edward@cassetteta.pe>
2014-07-15 10:46:25 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zoo-event "2.0.1"
+(defproject zoo-event "2.0.2"
   :description "Live Zooniverse Classifications"
   :url "http://github.com/zooniverse/zoo-events"
   :license {:name "Apache Public License v2"
